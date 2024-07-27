d70b4533fd049a14806755fb151464d5143683d1
Rishi <rishisrivastava2706@gmail.com>
2024-07-27 16:53:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import {queryClient} from './util/http.js'
+import Events from './components/Events/Events.jsx';
+import EventDetails from './components/Events/EventDetails.jsx';
+import NewEvent from './components/Events/NewEvent.jsx';
+import EditEvent, {loader as eventLoader} from './components/Events/EditEvent.jsx';
+import {  QueryClientProvider } from '@tanstack/react-query';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/events" />,
+  },
+  {
+    path: '/events',
+    element: <Events />,
+
+    children: [
+      {
+        path: '/events/new',
+        element: <NewEvent />,
+      },
+    ],
+  },
+  {
+    path: '/events/:id',
+    element: <EventDetails />,
+    children: [
+      {
+        path: '/events/:id/edit',
+        element: <EditEvent />,
+        loader:eventLoader,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <QueryClientProvider client={queryClient}><RouterProvider router={router} /></QueryClientProvider>;
+}
+
+export default App;
---
+++ src/components/Events/EditEvent.jsx
@@ -0,0 +1,82 @@
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import Modal from '../UI/Modal.jsx';
+import EventForm from './EventForm.jsx';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+
+export default function EditEvent() {
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const {data, isError, error} = useQuery({
+    queryKey:['events', params.id],
+    queryFn:({signal})=> fetchEvent({signal, id:params.id}),
+    staleTime: 10000
+  })
+
+  const {mutate} =useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data)=>{
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({queryKey:['events',params.id]})
+      const previousEvent = queryClient.getQueryData(['events',params.id]);
+
+      queryClient.setQueryData(['events',params.id], newEvent );
+
+      return{ previousEvent }
+    },
+    onError:(error, data, context)=>{
+      queryClient.setQueryData(['events', params.id], content.previousEvent)
+    },
+    onSettled:()=>{
+      queryClient.invalidateQueries(['events', params.id])
+    }
+  })
+
+  function handleSubmit(formData) {
+    mutate({id:params.id, event:formData})
+    navigate('../')
+  }
+
+  function handleClose() {
+    navigate('../');
+  }
+
+  let content;
+
+  if(isError){
+    content = <><ErrorBlock title='Failed to load event' message={error.info?.message || 'Failed to load event. Please check your inputs and try again later'}/>
+    <div className="form-actions">
+      <Link to='../' className='button'>
+         Okay
+      </Link>
+    </div>
+    </>
+  }
+
+  if(data){
+    content =  <EventForm inputData={data} onSubmit={handleSubmit}>
+    <Link to="../" className="button-text">
+      Cancel
+    </Link>
+    <button type="submit" className="button">
+      Update
+    </button>
+  </EventForm>
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+     {content}
+    </Modal>
+  );
+}
+
+export function loader({params}){
+  return  queryClient.fetchQuery({
+    queryKey:['events', params.id],
+    queryFn:({signal})=> fetchEvent({signal, id:params.id})
+  })
+}
